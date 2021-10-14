5a775cb7ad5102ea95f14db0208a2436fe4773b4
ShamerD <shomax31@gmail.com>
2021-10-14 21:48:32 +0300

    add lstm model

--- hw_asr/model/__init__.py
+++ hw_asr/model/__init__.py
@@ -1,5 +1,6 @@
-from hw_asr.model.baseline_model import BaselineModel
+from hw_asr.model.baseline_model import BaselineModel, BaselineLSTM
 
 __all__ = [
-    "BaselineModel"
+    "BaselineModel",
+    "BaselineLSTM"
 ]
--- hw_asr/model/baseline_model.py
+++ hw_asr/model/baseline_model.py
@@ -21,3 +21,22 @@ class BaselineModel(BaseModel):
 
     def transform_input_lengths(self, input_lengths):
         return input_lengths  # we don't reduce time dimension here
+
+
+class BaselineLSTM(BaseModel):
+    def __init__(self, n_feats, n_class, hidden_size=256, num_layers=3, *args, **kwargs):
+        super().__init__(n_feats, n_class, *args, **kwargs)
+        self.lstm = nn.LSTM(
+            input_size=n_feats,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+            *args, **kwargs
+        )
+        self.fc = nn.Linear(hidden_size, n_class, *args, **kwargs)
+
+    def forward(self, spectrogram, *args, **kwargs):
+        return {"logits": self.fc(self.lstm(spectrogram))}
+
+    def transform_input_lengths(self, input_lengths):
+        return input_lengths  # we don't reduce time dimension here
