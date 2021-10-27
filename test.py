ea3059db38761f80e8e761dacbe7c0e164a4404c
ShamerD <shomax31@gmail.com>
2021-10-27 23:37:17 +0300

    fix test config

--- test.py
+++ test.py
@@ -192,7 +192,5 @@ if __name__ == "__main__":
         }
 
     assert config.config.get("data", {}).get("test", None) is not None
-    config["data"]["test"]["batch_size"] = args.batch_size
-    config["data"]["test"]["n_jobs"] = args.n_jobs
 
     main(config, args.output)
